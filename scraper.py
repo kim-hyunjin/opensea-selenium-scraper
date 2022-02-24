36a65e1e7fc1a81473f2a8335f13fa962b21333a
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-24 19:51:02 +0900

    토큰 정보 json으로 만들기 완료

--- scraper.py
+++ scraper.py
@@ -6,13 +6,30 @@ from selenium.webdriver.common.by import By
 import time
 import requests
 from PIL import Image # pillow
+import json
+import random
+from faker import Faker
+from datetime import datetime
 
+fake = Faker()
 numOfAssets = int(input("토큰 개수"))
+collectionId = input("컬렉션 ID")
+tokenType = input("토큰type(erc721, erc1155)")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get('https://opensea.io/collection/mfers')
-driver.implicitly_wait(5) # seconds
+def main():
+    driver.get('https://opensea.io/collection/mfers')
+    driver.implicitly_wait(5) # seconds
+    try:
+        tokenUrlList = getTokenList()
+        for i in range(numOfAssets):
+            makeToken(tokenUrlList[i])
+    finally:
+        driver.quit()
+    
+    while(True):
+        pass
 
 def getCollectionInfo():
     pass
@@ -36,21 +53,74 @@ def getTokenList():
     
     return tokenUrls
 
-def getTokenInfo(url):
+def makeToken(url):
     driver.get(url)
     driver.implicitly_wait(3)
+    img = getImage()
+    print(img)
+    token = getTokenInfo()
+    print(json.dumps(token))
+
+
+def getImage():
     imgElement = driver.find_element(By.CLASS_NAME, "Image--image")
     imgUrl = imgElement.get_attribute("src")
     imgRes = requests.get(imgUrl)
-    img = Image.open(BytesIO(imgRes.content))
-    print(img)
+    return Image.open(BytesIO(imgRes.content))
 
-try:
-    tokenUrlList = getTokenList()
-    for i in range(numOfAssets):
-        getTokenInfo(tokenUrlList[i])
-finally:
-    driver.quit()
+def getTokenInfo():
+    token = {}
+    token["collection_id"] = collectionId
+    token["name"] = driver.find_element(By.CLASS_NAME, "item--title").text
+    token["description"] = driver.find_element(By.CSS_SELECTOR, ".item--description-text>span").text
+    token["type"] = tokenType
+    token["total_count"] = getTotalCount()
+    token["royalties"] = random.randrange(1, 20)
+    token["sale"] = getSale()
+    token["traits"] = getTraits()
 
-while(True):
-    pass
+    return token
+
+
+def getTotalCount():
+    if tokenType == "erc721":
+        return 1
+    else:
+        return random.randrange(1, 5)
+
+def getSale():
+    saleType = randomSaleType()
+    sale = {}
+    sale["sale_type"] = saleType
+    sale["currency"] = "eth"
+    if saleType == "fixed":
+        sale["price"] = randomPrice()
+    elif saleType == "time":
+        sale["start_price"] = randomPrice()
+        sale["start_at"] = datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+        sale["end_at"] = randomDateWithinMonth().strftime("%Y-%m-%dT%H:%M:%SZ")
+    return sale
+
+def getTraits():
+    traits = []
+    properties = driver.find_elements(By.CSS_SELECTOR, ".item--properties>a")
+    for prop in properties:
+        trait = {}
+        trait["key"] = prop.find_element(By.CLASS_NAME, "Property--type").get_attribute('innerHTML')
+        trait["value"] = prop.find_element(By.CLASS_NAME, "Property--value").get_attribute('innerHTML')
+        traits.append(trait)
+    return traits
+
+def randomSaleType():
+    saleType = ["fixed", "auction", "time"]
+    return random.choice(saleType)
+
+def randomPrice():
+    return round(random.uniform(0.01, 100.0), 2)
+
+def randomDateWithinMonth():
+    return fake.date_time_between(start_date='now', end_date="+30d")
+    
+
+if __name__ == "__main__":
+    main()
