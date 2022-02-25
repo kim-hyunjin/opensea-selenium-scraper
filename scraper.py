e13b8750cc8bf6b99bf68d4c872c60dbe9f47ae0
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-25 14:56:51 +0900

    opensea scraper 기능 구현 완료(scrape & save to server)

--- scraper.py
+++ scraper.py
@@ -10,31 +10,130 @@ import json
 import random
 from faker import Faker
 from datetime import datetime
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import logging
+from dotenv import load_dotenv
+import os
+
+logging.basicConfig(
+  level = logging.INFO
+)
+
+load_dotenv()
 
 fake = Faker()
-numOfAssets = int(input("토큰 개수"))
-collectionId = input("컬렉션 ID")
-tokenType = input("토큰type(erc721, erc1155)")
-
+authKey = input('인증키')
+numOfCollections = int(input('컬렉션 수'))
+maxNumOfAssets = int(input("최대 토큰 개수"))
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def main():
-    driver.get('https://opensea.io/collection/mfers')
-    driver.implicitly_wait(5) # seconds
     try:
-        tokenUrlList = getTokenList()
-        for i in range(numOfAssets):
-            makeToken(tokenUrlList[i])
+        scrapeCollection()
     finally:
         driver.quit()
-    
-    while(True):
-        pass
 
-def getCollectionInfo():
-    pass
+# 컬렉션 관련
+def scrapeCollection():
+    collectionUrls = getCollectionUrls()
+    for i in range(numOfCollections):
+        collectionInfo = createCollection(collectionUrls[i])
+        if collectionInfo == None:
+            continue
+        scrapeTokens(collectionInfo)
 
-def getTokenList():
+def getCollectionUrls():
+    driver.get(rancomCategory())
+    driver.implicitly_wait(1)
+    for _ in range(random.randrange(0, 10)):
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    driver.implicitly_wait(5)
+    collectionUrls = []
+    while True:
+        collections = driver.find_elements(By.CSS_SELECTOR, "a.CarouselCard--main")
+        for collection in collections:
+            collectionUrls.append(collection.get_attribute('href'))
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        if len(collectionUrls) > numOfCollections: break
+    return collectionUrls
+
+def rancomCategory():
+    driver.get('https://opensea.io/explore-collections')
+    driver.implicitly_wait(5)
+    category = driver.find_element(By.CSS_SELECTOR, "#main ul")
+    tabLinks = category.find_elements(By.CSS_SELECTOR, "li > a")
+    tabUrls = []
+    for link in tabLinks:
+        tabUrls.append(link.get_attribute('href'))
+    return random.choice(tabUrls)
+
+def createCollection(url):
+    logging.info('collection url: {}'.format(url))
+    driver.get(url)
+    driver.implicitly_wait(5)
+    collectionImg = driver.find_element(By.CSS_SELECTOR, ".CollectionHeader--collection-image > img")
+    imgUrl = collectionImg.get_attribute('src')
+    img = getImage(imgUrl)
+
+    itemStatus = driver.find_element(By.CLASS_NAME, 'CollectionStatsBar--bottom-bordered div[tabIndex="-1"]')
+    try:
+        maxItemCnt = int(itemStatus.get_attribute('innerHTML'))
+    except:
+        maxItemCnt = maxNumOfAssets
+
+    collectionInfo = {}
+    collectionName = driver.find_element(By.TAG_NAME, "h1").text
+    collectionInfo["name"] = collectionName
+    collectionInfo["symbol"] = collectionName[:3].upper()
+    try:
+        desc = driver.find_element(By.CSS_SELECTOR, "CollectionHeader--description > span")
+        collectionInfo["description"] = desc.text
+    except:
+        collectionInfo["description"] = ""
+
+    collectionInfo["type"] = random.choice(["erc721", "erc1155"])
+
+    res = sendCollectionToServer(img, collectionInfo)
+    if res.status_code != 200:
+        logging.warning('컬렉션 생성 실패')
+        return None
+
+    resBody = res.json()
+    collectionInfo["collection_id"] = resBody["collection"]["id"]
+    collectionInfo["item_cnt"] = random.randrange(1, min([maxNumOfAssets, maxItemCnt]))
+    return collectionInfo
+
+def sendCollectionToServer(img: Image, collection):
+    url = os.getenv('COLLECTION_API_URL')
+    imgIO = BytesIO()
+    img.save(imgIO, img.format)
+    img_format = img.format.lower()
+    encoded = MultipartEncoder(
+        fields={
+            'thumbnailImage': ("thumbnail.{}".format(img_format), imgIO, 'image/{}'.format(img_format)),
+            'json': json.dumps(collection)
+        }
+    )
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': encoded.content_type,
+        'Authorization': "Bearer {}".format(authKey)
+    }
+    res = requests.post(url, headers=headers, data=encoded)
+    logging.info('collection {} {}'.format(collection["name"], res.status_code))
+    return res
+
+# 토큰 관련
+def scrapeTokens(collectionInfo):
+    logging.info('token scrape')
+    urlsMoreThanItemCnt = getTokenUrls(collectionInfo["item_cnt"])
+    for i in range(collectionInfo["item_cnt"]):
+        isMakeToken = createToken(urlsMoreThanItemCnt[i], collectionInfo["collection_id"], collectionInfo["type"])
+        if isMakeToken == False:
+            logging.warning('token 만들기 실패')
+            break
+
+def getTokenUrls(itemCnt):
     tokenUrls = []
 
     viewToggleButton = driver.find_elements(By.CLASS_NAME, "AssetSearchView--toggle-buttons")[1]
@@ -48,41 +147,64 @@ def getTokenList():
             link = asset.find_element(By.CLASS_NAME, "Asset--anchor")
             tokenUrls.append(link.get_attribute('href'))
         driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        print(len(tokenUrls))
-        if len(tokenUrls) > numOfAssets: break
+        if len(tokenUrls) > itemCnt: break
     
     return tokenUrls
 
-def makeToken(url):
+def createToken(url, collectionId, tokenType):
     driver.get(url)
-    driver.implicitly_wait(3)
-    img = getImage()
-    print(img)
-    token = getTokenInfo()
-    print(json.dumps(token))
+    driver.implicitly_wait(5)
+    img = getTokenImage()
+    if img == None:
+        logging.warning('토큰 이미지 가져오기 실패')
+        return False
+    token = getTokenInfo(collectionId, tokenType)
+    sendTokenToServer(img, token)
 
+def sendTokenToServer(img: Image, token):
+    url = os.getenv('TOKEN_API_URL')
+    imgIO = BytesIO()
+    img.save(imgIO, img.format)
+    img_format = img.format.lower()
+    encoded = MultipartEncoder(
+        fields={
+            'contentImage': ("test.{}".format(img_format), imgIO, 'image/{}'.format(img_format)),
+            'json': json.dumps(token)
+        }
+    )
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': encoded.content_type,
+        'Authorization': "Bearer {}".format(authKey)
+    }
+    res = requests.post(url, headers=headers, data=encoded)
+    logging.info('token {} {}'.format(token["name"], res.status_code))
 
-def getImage():
-    imgElement = driver.find_element(By.CLASS_NAME, "Image--image")
-    imgUrl = imgElement.get_attribute("src")
-    imgRes = requests.get(imgUrl)
-    return Image.open(BytesIO(imgRes.content))
+def getTokenImage():
+    try:
+        imgElement = driver.find_element(By.CLASS_NAME, "Image--image")
+        imgUrl = imgElement.get_attribute("src")
+        return getImage(imgUrl)
+    except:
+        return None
 
-def getTokenInfo():
+def getTokenInfo(collectionId, tokenType):
     token = {}
     token["collection_id"] = collectionId
     token["name"] = driver.find_element(By.CLASS_NAME, "item--title").text
-    token["description"] = driver.find_element(By.CSS_SELECTOR, ".item--description-text>span").text
+    try:
+        token["description"] = driver.find_element(By.CSS_SELECTOR, ".item--description-text>span").text
+    except:
+        token["description"] = ""
     token["type"] = tokenType
-    token["total_count"] = getTotalCount()
+    token["total_count"] = getTotalCount(tokenType)
     token["royalties"] = random.randrange(1, 20)
     token["sale"] = getSale()
     token["traits"] = getTraits()
 
     return token
 
-
-def getTotalCount():
+def getTotalCount(tokenType):
     if tokenType == "erc721":
         return 1
     else:
@@ -110,6 +232,11 @@ def getTraits():
         trait["value"] = prop.find_element(By.CLASS_NAME, "Property--value").get_attribute('innerHTML')
         traits.append(trait)
     return traits
+
+def getImage(url):
+    imgRes = requests.get(url)
+    img = Image.open(BytesIO(imgRes.content))
+    return img
 
 def randomSaleType():
     saleType = ["fixed", "auction", "time"]
